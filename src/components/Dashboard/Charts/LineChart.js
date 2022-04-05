bd08a84e7fc79790d00f66c5328023f6c444c4e0
UmmeHania <87921374+UmmeHania@users.noreply.github.com>
2022-04-05 22:52:47 +0600

    charts recreated

--- src/components/Dashboard/Charts/LineChart.js
+++ src/components/Dashboard/Charts/LineChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bar, BarChart, Line, LineChart, Pie, PieChart, RadialBarChart, Tooltip, XAxis, YAxis } from "recharts";
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, RadialBarChart, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const MyLineChart = () => {
     const data = [
@@ -41,24 +41,93 @@ const MyLineChart = () => {
         }
     ]
     return (
-        <div className='m-32 mt-10'>
-            <div className='mb-5'>
-                <h1 className='text-center font-bold text-xl'>Monthly Selling</h1>
-                <LineChart width={800} height={400} data={data}  >
-                    <Line dataKey={"sell"}></Line>
-                    <XAxis dataKey={"month"}></XAxis>
-                    <YAxis></YAxis>
+        <div className='mt-10 grid grid-cols-2 gap-4 justify-items-center shadow-lg rounded-2xl bg-white p-4'>
+            <div className='shadow-lg rounded-2xl'>
+                <h1 className='text-center font-bold text-xl mb-3'>Monthly Revenue Vs Sell</h1>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
                     <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="revenue" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
+
+                    <YAxis ></YAxis>
                 </LineChart>
             </div>
-            <div>
-                <h1 className='text-center font-bold text-xl'>Investment in Monthly Basis</h1>
-                <BarChart width={800} height={400} data={data}>
-                    <Bar dataKey={"investment"} fill="#8884d8" />
-                    <XAxis dataKey={"month"}></XAxis>
-                    <YAxis ></YAxis>
+            <div className='shadow-lg rounded-2xl'>
+                <h1 className='text-center font-bold text-xl mb-3'>Investment in Monthly Basis</h1>
+
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
                     <Tooltip />
+                    <Legend />
+                    <Bar dataKey="revenue" fill="#8884d8" />
+                    <Bar dataKey="investment" fill="#82ca9d" />
                 </BarChart>
+
+            </div>
+            <div className='shadow-lg rounded-2xl'>
+                <h1 className='text-center font-bold text-xl mt-5'>Scatter Chart</h1>
+                <ScatterChart
+                    width={400}
+                    height={400}
+                    margin={{
+                        top: 20,
+                        right: 20,
+                        bottom: 20,
+                        left: 20,
+                    }}
+                >
+                    <CartesianGrid />
+                    <XAxis type="number" dataKey="sell" />
+                    <YAxis type="number" dataKey="investment" />
+                    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                    <Scatter name="A school" data={data} fill="#8884d8" />
+                </ScatterChart>
+            </div>
+            <div className='shadow-lg rounded-2xl mt-5'>
+                <h1 className='text-center font-bold text-xl'>Area Chart</h1>
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="sell" stroke="#8884d8" fill="#8884d8" />
+                </AreaChart>
             </div>
         </div>
     );
